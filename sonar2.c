b21d91bb28eeeb6cb3d36a2d7184e0dc8dc457d5
JingyaHuangGrace <128197159+JingyaHuangGrace@users.noreply.github.com>
2024-04-15 23:35:19 -0700

    Update sonar2.c

--- sonar2.c
+++ sonar2.c
@@ -8,7 +8,7 @@ void compare(int a, int b) {
     if (a > b) {
         printf("%d > %d\n", a, b);
     } else if (a == b) {
-        printf("%d = %d\n", a, b);
+        printf("%d > %d\n", a, b);
     } else {
         printf("%d < %d\n", a, b);
     }
